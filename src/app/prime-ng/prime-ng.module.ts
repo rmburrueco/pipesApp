6b9dffe73d2be8128699690e71f7007b574df5db
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2022-06-28 13:42:48 +0200

    Se añade el menuBar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 
 //PrimeNG
 import { ButtonModule } from 'primeng/button';
-import {CardModule} from 'primeng/card'
+import { CardModule } from 'primeng/card'
+import { MenubarModule } from 'primeng/menubar'
 
 @NgModule({
   declarations: [],
   exports: [
     ButtonModule,
-    CardModule
+    CardModule,
+    MenubarModule
   ]
 })
 export class PrimeNgModule { }
