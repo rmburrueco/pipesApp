3f611c2d5a091367e39e5e565ec3ea8c58ac0035
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2022-06-30 14:22:23 +0200

    Se personaliza el menu de PrimeNg para enrutar

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -16,28 +16,17 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-          label: 'File',
-          items: [{
-                  label: 'New', 
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
-      },
-      {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-          ]
+        label: 'Pipes de Angular',
+        icon : 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon : 'pi pi-align-left',
+            routerLink: ''
+          }
+        ]
       }
-  ];
+    ];
   }
 
 }
